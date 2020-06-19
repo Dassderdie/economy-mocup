a650e26e2887e1f3fcedbfdb7978021c2050b952
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2020-06-19 16:28:20 +0200

    Add placeholder to landing page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { companyData } from 'src/app/shared/data/company-data';
 
 @Component({
     templateUrl: './landing-page.component.html',
     styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
+    // to use it in template
+    public company = companyData;
     constructor() {}
 
     ngOnInit(): void {}
